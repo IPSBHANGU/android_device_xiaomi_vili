7d4866f6410c07c38a7647e76124cd6762e319c5
Arian <arian.kulmer@web.de>
2021-10-13 23:50:11 +0200

    vili: Set dtbo and userdata partition sizes
    Change-Id: I9e61f6abcbefba66af92b3967df80fc74b084753
    Signed-off-by: IPSBHANGU <inderpreetbhangu2@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -15,5 +15,9 @@ TARGET_BOOTLOADER_BOARD_NAME := vili
 # Kernel
 TARGET_KERNEL_CONFIG += vendor/vili_QGKI.config
 
+# Partitions
+BOARD_DTBOIMG_PARTITION_SIZE := 25165824
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 113254576128
+
 # Include proprietary files
 include vendor/xiaomi/vili/BoardConfigVendor.mk
