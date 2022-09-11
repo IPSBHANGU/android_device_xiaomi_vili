5bf2e21496583e010d4206571814728da22bda2d
Giovanni Ricca <giovanniricca@proton.me>
2022-09-11 22:08:03 +0200

    vili: Include device specific kernel config fragment
    Change-Id: Id5cacc4f1caf8edb3defc1f793a0c501039269f1
    Signed-off-by: IPSBHANGU <inderpreetbhangu2@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,5 +12,8 @@ include device/xiaomi/sm8350-common/BoardConfigCommon.mk
 # Board
 TARGET_BOOTLOADER_BOARD_NAME := vili
 
+# Kernel
+TARGET_KERNEL_CONFIG += vendor/vili_QGKI.config
+
 # Include proprietary files
 include vendor/xiaomi/vili/BoardConfigVendor.mk
